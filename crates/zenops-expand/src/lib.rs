c97fa61a0f7cf1a031dba32293a42d1e69541a1f
Bjorn Ove Hay Andersen <bjrnove@gmail.com>
2026-04-21 09:29:37 +0200

    Fixed rustdoc compile issue

--- crates/zenops-expand/src/lib.rs
+++ crates/zenops-expand/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs)]
 
 //! String newtype that carries `${name}` placeholders which must be expanded
